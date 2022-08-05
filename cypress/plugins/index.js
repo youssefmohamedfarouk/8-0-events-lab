7cd95765f73a7c78381b45bb0df30a00f0d053a4
Karolin Rafalski <kr2174@caa.columbia.edu>
2022-08-05 17:56:15 -0400

    set cypress version

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -16,5 +16,9 @@
  * @type {Cypress.PluginConfig}
  */
 module.exports = (on, config) => {
+  // https://github.com/bahmutov/cypress-watch-and-reload
   require("cypress-watch-and-reload/plugins")(config);
+  // IMPORTANT: return the config object
+  // because the plugin might have changed it
+  return config;
 };
